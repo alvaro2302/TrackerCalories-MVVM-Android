db1718611e1318260d9c8585edb8be91bc9e631c
Acuiza <Acuiza@hypernovalabs.com>
2026-02-01 23:01:41 -0400

    add component UnitDisplay for show the number of calories or other unit values

---
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/components/UnitDisplay.kt
@@ -0,0 +1,59 @@
+package com.example.trackercaloriesapp.presentation.tracker_overview.components
+
+import android.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
+import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+
+@Composable
+fun UnitDiplay(
+    amount: Int,
+    modifier: Modifier = Modifier,
+    unit: String,
+    amountTextSize: TextUnit = 20.sp,
+    amountColor: Color = MaterialTheme.colorScheme.onBackground,
+    unitTextSize: TextUnit = 14.sp,
+    unitColor: Color = MaterialTheme.colorScheme.onBackground
+
+) {
+    val spacing = LocalSpacing.current
+    Row (
+        modifier= modifier
+    ) {
+        Text(
+            text = amount.toString(),
+            style = MaterialTheme.typography.titleLarge,
+            fontSize = amountTextSize,
+            color = amountColor,
+            modifier = Modifier.alignBy(LastBaseline)
+        )
+        Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
+        Text(
+            text = unit,
+            style = MaterialTheme.typography.titleLarge,
+            fontSize = amountTextSize,
+            color = amountColor,
+            modifier = Modifier.alignBy(LastBaseline)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun UnitDisplayPreview() {
+    PlatziCaloriesTheme {
+        UnitDiplay(amount = 100, unit = "kg")
+    }
+}
