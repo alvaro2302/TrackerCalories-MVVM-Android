1749b358c49f5a71cd7f21f6ded823ee7632a770
Acuiza <Acuiza@hypernovalabs.com>
2026-02-02 15:04:28 -0400

    add component DaySelector

---
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/components/DaySelector.kt
@@ -0,0 +1,65 @@
+package com.example.trackercaloriesapp.presentation.tracker_overview.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+import java.time.LocalDate
+
+@Composable
+fun DaySelector(
+    date: LocalDate,
+    onPreviousDayClick: () -> Unit,
+    onNextDayClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = onPreviousDayClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Previous Day"
+            )
+        }
+        Text(
+            text = ParseDateText(date= LocalDate.now()),
+            style = MaterialTheme.typography.titleLarge
+        )
+        IconButton(
+            onClick = onNextDayClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "Next Day"
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DaySelectorPreview() {
+    PlatziCaloriesTheme {
+        DaySelector(
+            date = LocalDate.now(),
+            onPreviousDayClick = {},
+            onNextDayClick = {}
+        )
+    }
+}
