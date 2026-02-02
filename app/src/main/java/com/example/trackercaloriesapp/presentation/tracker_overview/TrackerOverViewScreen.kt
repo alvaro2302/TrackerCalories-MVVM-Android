5515e8f01cca96b5043e4866f9758aeeed632c5d
Acuiza <Acuiza@hypernovalabs.com>
2026-02-02 00:03:12 -0400

    add components to TrackerOverViewScreen

--- app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewScreen.kt
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -3,26 +3,60 @@ package com.example.trackercaloriesapp.presentation.tracker_overview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.trackercaloriesapp.presentation.tracker_overview.components.NutrientsHeaders
+import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
+import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
 
 @Composable
 fun TrackerOverViewScreen(
     trackerOverViewModel: TrackerOverViewModel = hiltViewModel()
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize()
+    val spacing = LocalSpacing.current
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().
+        padding(spacing.spaceMedium)
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Tracker Overview Screen")
+        item {
+            NutrientsHeaders()
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
+    }
+}
+@Composable
+fun TrackerOverViewScreenTest(
+) {
+    val spacing = LocalSpacing.current
+    LazyColumn(
+            modifier = Modifier.fillMaxSize().
+            padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeaders()
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+    }
+
+}
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun TrackerOverViewScreenPreview() {
+    PlatziCaloriesTheme {
+        TrackerOverViewScreenTest()
     }
 }
