28aa12078b1810b818dd8fb0eb28df2bdf52049c
Acuiza <Acuiza@hypernovalabs.com>
2026-02-02 10:15:39 -0400

    remove imports that not use

--- app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewScreen.kt
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -1,16 +1,11 @@
 package com.example.trackercaloriesapp.presentation.tracker_overview
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
