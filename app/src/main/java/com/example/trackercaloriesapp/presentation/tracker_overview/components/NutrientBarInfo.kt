1d5e8fc914b98b0ceec6f7f911e2f34ce80a6e85
Acuiza <Acuiza@hypernovalabs.com>
2026-02-01 23:28:08 -0400

    add component NutrientBarInfo for show value and units

---
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/components/NutrientBarInfo.kt
@@ -0,0 +1,109 @@
+package com.example.trackercaloriesapp.presentation.tracker_overview.components
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.ModifierLocalMap
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.room.util.TableInfo
+import com.example.trackercaloriesapp.R
+import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+
+
+@Composable
+fun Nutrient(
+    value: Int,
+    goal: Int,
+    name: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    val background = MaterialTheme.colorScheme.onSurfaceVariant
+    val goalExceededColor = MaterialTheme.colorScheme.error
+    val angleRatio = remember {
+        Animatable(initialValue = 0f)
+    }
+    LaunchedEffect(key1 = value) {
+        angleRatio.animateTo(
+            targetValue = if (goal> 0) {
+                value/goal.toFloat()
+            } else {
+                0F
+            },
+            animationSpec = tween(
+                durationMillis = 300
+            )
+        )
+    }
+    Box(
+        modifier= modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxWidth()
+                .aspectRatio(ratio = 1f)
+
+        ){
+            drawRoundRect(
+                color = if (value <= goal ) background else goalExceededColor,
+                size = size,
+                cornerRadius = CornerRadius.Zero
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            UnitDisplay(
+                amount = value,
+                unit = stringResource(id = R.string.grams),
+                amountColor = if (value <= goal) {
+                    MaterialTheme.colorScheme.onSecondary
+                    } else  goalExceededColor,
+                unitColor = if(value <= goal) {
+                    MaterialTheme.colorScheme.onSecondary
+                    } else goalExceededColor
+
+            )
+            Text(
+                text = name,
+                color = if (value <= goal) {
+                    MaterialTheme.colorScheme.onSecondary
+                    } else goalExceededColor,
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Light
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NutrientPreview() {
+    PlatziCaloriesTheme {
+        Nutrient(value = 100, goal = 200, name = "Carbohidratos",
+            color = Color.Red, modifier = Modifier.size(90.dp)
+        )
+    }
+}
+
+
