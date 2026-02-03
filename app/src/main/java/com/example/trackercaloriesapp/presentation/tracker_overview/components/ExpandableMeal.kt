04dcdd32e3345074cef6bd07a257b5493a7678b8
Acuiza <Acuiza@hypernovalabs.com>
2026-02-02 22:09:58 -0400

    add component for show or hidden information for specific food from calories and carb and proteins

---
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/components/ExpandableMeal.kt
@@ -0,0 +1,136 @@
+package com.example.trackercaloriesapp.presentation.tracker_overview.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.example.trackercaloriesapp.presentation.tracker_overview.model.Meal
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.ModifierLocal
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import coil3.Image
+import com.example.trackercaloriesapp.R
+import com.example.trackercaloriesapp.app.tracker.model.MealType
+import com.example.trackercaloriesapp.core.domain.util.UiText
+import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
+import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+
+@Composable
+fun ExpandableMeal(
+    meal: Meal,
+    onToggleClick: () -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val spacing = LocalSpacing.current
+    val context = LocalContext.current
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().clickable {
+
+            }.padding(spacing.spaceMedium),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = meal.drawableRes),
+                contentDescription = meal.name.asString(context)
+            )
+            Spacer(modifier= Modifier.width(spacing.spaceMedium))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = meal.name.asString(context),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Icon(
+                        imageVector = if(meal.isExpanded){
+                            Icons.Default.KeyboardArrowUp
+                        } else Icons.Default.KeyboardArrowDown,
+                        contentDescription = if (meal.isExpanded) {
+                            stringResource(R.string.collapse)
+                        } else stringResource(R.string.extend)
+                    )
+
+                }
+                Spacer(modifier = modifier.height(spacing.spaceSmall))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    UnitDisplay(
+                        amount = meal.calories,
+                        unit = stringResource(R.string.kcal),
+                        amountTextSize = 30.sp
+                    )
+                    Row {
+                        NutrientInfo(
+                            name = stringResource(R.string.carbs),
+                            amount = meal.carbs,
+                            unit = stringResource(R.string.grams)
+                        )
+                        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                        NutrientInfo(
+                            name = stringResource(R.string.carbs),
+                            amount = meal.proteins,
+                            unit = stringResource(R.string.grams)
+                        )
+                        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                        NutrientInfo(
+                            name = stringResource(R.string.grams),
+                            amount = meal.carbs,
+                            unit = stringResource(R.string.grams)
+                        )
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ExpandableMealPreview() {
+    PlatziCaloriesTheme {
+        ExpandableMeal(
+            meal = Meal(
+                name = UiText.StringResources(R.string.breakfast),
+                drawableRes = R.drawable.breakfast_02,
+                calories = 100,
+                mealType = MealType.Breakfast,
+                carbs = 50,
+                proteins = 100,
+                fat = 10
+            ),
+            onToggleClick = {},
+            content = {}
+        )
+    }
+}
