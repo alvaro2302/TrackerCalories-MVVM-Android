415b6493ebf645c75a3bd3a97814412b4e60272c
Acuiza <Acuiza@hypernovalabs.com>
2026-02-03 15:09:58 -0400

    call method use case for get data from openFoodApi

--- app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewModel.kt
+++ app/src/main/java/com/example/trackercaloriesapp/presentation/tracker_overview/TrackerOverViewModel.kt
@@ -1,15 +1,26 @@
 package com.example.trackercaloriesapp.presentation.tracker_overview
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.trackercaloriesapp.app.tracker.usercase.TrackerUseCase
 import com.example.trackercaloriesapp.core.domain.preferences.Preferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TrackerOverViewModel @Inject constructor(
-    preferences: Preferences
+    preferences: Preferences,
+    private val trackerUseCase: TrackerUseCase
 ) : ViewModel() {
     init {
         preferences.saveShouldShowOnboarding(false)
+        executeFoodApi()
+    }
+
+    fun executeFoodApi() {
+        viewModelScope.launch {
+            trackerUseCase.searchFoodUseCase("egg")
+        }
     }
 }
